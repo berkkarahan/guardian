3614f3e44197f1207a3e03b56430da7fb1cc0c79
“berkkarahan” <berkkarahan00@gmail.com>
2020-03-29 23:41:53 +0300

    new app start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
       // args: "one two",
-      instances: 2,
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
