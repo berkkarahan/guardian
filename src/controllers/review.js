4ab8c08180514b11a7ecf551be103633c1d00c5c
“berkkarahan” <berkkarahan00@gmail.com>
2020-04-25 17:27:36 +0300

    comment

--- src/controllers/review.js
+++ src/controllers/review.js
@@ -393,6 +393,7 @@ const deleteSubDocument = tryCatch(async (req, res, next) => {
   const review = await Review.findOne({ uuid: uuid });
   await review[subdoc].remove();
   await review.save();
+  // remove review from response later
   res.status(200).json({ review: review });
 });
 
